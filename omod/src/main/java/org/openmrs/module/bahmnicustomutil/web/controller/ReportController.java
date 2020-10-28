86a71f0dc3bc265503477fcf95aca8b84762e932
atuar123 <atuar.cse@gmail.com>
2020-10-28 12:28:14 +0600

    file name access

--- omod/src/main/java/org/openmrs/module/bahmnicustomutil/web/controller/ReportController.java
+++ omod/src/main/java/org/openmrs/module/bahmnicustomutil/web/controller/ReportController.java
@@ -1,9 +1,13 @@
 package org.openmrs.module.bahmnicustomutil.web.controller;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.File;
+import java.io.IOException;
 
 @Controller
 public class ReportController {
@@ -12,6 +16,20 @@ public class ReportController {
 	@RequestMapping(value = "/module/bahmnicustomutil/callMe", method = RequestMethod.GET)
 	public String callMe() {
 		return "Hi Atuar";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/module/bahmnicustomutil/getFileName", method = RequestMethod.GET)
+	public String getFileName() {
+		File file = null;
+		try {
+			file = new ClassPathResource("customer.jrxml").getFile();
+		}
+		catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+		String name = file.getName();
+		return name;
 	}
 	
 	@RequestMapping(value = "/module/bahmnicustomutil/index", method = RequestMethod.GET)
